709ae933a371febc381cce4aeea3e2cf35f5b4d6
shakadak <nathanael.pineau@gmail.com>
2015-02-26 16:14:49 +0100

    updating ray creation

--- src/ray/new_ray.c
+++ src/ray/new_ray.c
@@ -15,5 +15,6 @@ t_ray	new_ray(t_camera const camera,
 	on_projection.x *= tan(camera.fov / 2.0);
 	on_projection.z = -1;
 	ray.dir = vec_from_vtx(vtx_from_coordinates(0, 0, 0), on_projection);
+	ray.dir = vec_normalize(ray.dir);
 	return (ray);
 }
