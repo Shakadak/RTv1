da8e9c155da61c366c2df71dc16d0d3109b79e1d
shakadak <nathanael.pineau@gmail.com>
2015-02-28 16:54:35 +0100

    starting cylinder intersection function

---
+++ src/shapes/intersec_cylinder.c
@@ -0,0 +1,15 @@
+#include "rtv1.h"
+
+double	intersec_cylinder(t_object const self, t_ray const ray)
+{
+	double	a;
+	double	b;
+	double	c;
+	t_vec	line;
+
+	line = vec_sub(ray.pos, self.pos);
+	a = vec_dot(ray.dir, ray.dir);
+	b = 2 * vec_dot(ray.dir, line);
+	c = vec_dot(line, line) - self.shape.sphere.radius * self.shape.sphere.radius;
+	return (solve_quadratic(a, b, c));
+}
