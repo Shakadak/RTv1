af7cafe702d7b5878003687f291922b79b1e62f7
shakadak <nathanael.pineau@gmail.com>
2015-03-04 15:49:01 +0100

    correcting returned value

--- src/util/solve_quadratic.c
+++ src/util/solve_quadratic.c
@@ -11,19 +11,18 @@ double	solve_quadratic(double const a, double const b, double const c)
 	if (delta < 0)
 		return (-1);
 	else if (delta == 0)
-	{
-		x0 = -0.5 * b / a;
-		x1 = x0;
-	}
+		return (x0 = -0.5 * b / a);
 	else
 	{
 		q = (b > 0) ? -0.5 * (b + sqrt(delta)) : -0.5 * (b - sqrt(delta));
 		x0 = q / a;
 		x1 = c / q;
+		if (x1 > 0.0 && x0 > 0.0)
+			return (x0 < x1 ? x0 : x1);
+		else if (x0 > 0.0)
+			return (x0);
+		else if (x1 > 0.0)
+			return (x1);
+		return (-1);
 	}
-	if (x0 < x1 && x0 > 0.0)
-		return (x0);
-	else if (x1 < x0 && x1 > 0.0)
-		return (x1);
-	return (-1);
 }
