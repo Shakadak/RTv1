6ab23075474fb1350c11313ca1fcc7aa6eef25df
shakadak <nathanael.pineau@gmail.com>
2015-03-06 14:38:12 +0100

    scaling matrix initialization

--- src/util/mtx_scale.c
+++ src/util/mtx_scale.c
@@ -2,4 +2,11 @@
 
 t_mtx	mtx_scale(double const x, double const y, double const z)
 {
+	t_mtx	scale;
+
+	scale = mtx_identity();
+	scale[0][0] = x;
+	scale[1][1] = y;
+	scale[2][2] = z;
+	return (scale);
 }
