35c8217ed48060a020f5cb7676eedeebf88ed26e
Nathanael <Shakadak@users.noreply.github.com>
2015-03-15 13:57:57 +0100

    Update main.c
    might not work

--- src/main/main.c
+++ src/main/main.c
@@ -3,29 +3,19 @@
 
 int	test(t_env *env)
 {
-	t_object	objects[2];
 	t_ray		ray;
 	int			x;
 	int			y;
-	t_pos		dim;
-	t_camera	camera;
 	int			dist;
 
-			ft_putendl("yol");
-	dim = new_pos(1366, 768, 0);
-	camera = new_camera(vtx_new(0, 1, 0, 1), vtx_new(0, 0, -1, 1));
-	objects[1] = new_cone(vtx_new(5, 1, 1, 1),
-			vtx_new(45 * M_PI / 180, 45 * M_PI / 180, 00 * M_PI / 180, 1),
-			vtx_new(0, 0, -105, 1),
-			new_color(0xFF, 0xFF, 0x88));
 	y = 0;
-	while (y < dim.y)
+	while (y < env->y)
 	{
 		x = 0;
-		while (x < dim.x)
+		while (x < env->x)
 		{
-			ray = new_ray(camera, new_pos(x, y, 0), dim);
-			dist = objects[1].intersec(ray_transform(ray, objects[1].pipe));
+			ray = new_ray(env->camera, new_pos(x, y, 0), dim);
+			dist = env->objects[1].intersec(ray_transform(ray, env->objects[1].pipe));
 			put_pixel_to_image(env->img, new_pixel(new_pos(x, y, 0), new_color(0, 0, (dist > 0 ? 200 : 0))));
 			++x;
 		}
@@ -39,6 +29,11 @@ int	main(void)
 {
 	t_env	env;
 
+	env.camera = new_camera(vtx_new(0, 1, 0, 1), vtx_new(0, 0, -1, 1));
+	env.objects[1] = new_cone(vtx_new(5, 1, 1, 1),
+			vtx_new(45 * M_PI / 180, 45 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(0, 0, -105, 1),
+			new_color(0xFF, 0xFF, 0x88));
 	env.mlx = new_mlx();
 	env.win = new_window(env.mlx, 1366, 768, "holy carp");
 	env.img = new_image(env.mlx, 1366, 768);
