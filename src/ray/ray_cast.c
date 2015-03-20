d5fcc2f29ac543adc600af421b00404e9e099f20
Nathanael <nathanael.pineau@gmail.com>
2015-03-20 19:13:51 +0100

    starting multiple objects gestion

---
+++ src/ray/ray_cast.c
@@ -0,0 +1,13 @@
+t_color	ray_cast(t_pos const pos,
+		t_camera const camera,
+		t_object const * const objects,
+		t_light const * const lights)
+{
+	t_ray		ray;
+	t_object	nearest;
+	double		distance;
+
+	distance = -1;
+	nearest = new_object
+	ray = new_ray(env->camera, new_pos(x, y, 0), env->img.dim);
+}
