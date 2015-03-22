4cc611fb9881d706359400ccdfb00dae6d82dc6c
shakadak <nathanael.pineau@gmail.com>
2015-03-22 17:47:09 +0100

    updating scene a bit

--- src/main/main.c
+++ src/main/main.c
@@ -31,10 +31,30 @@ int	main(void)
 
 	env.camera = new_camera(vtx_new(0, 1, 0, 1), vtx_new(0, 0, -1, 1), new_pos(1366, 768, 0));
 	ft_bzero(env.objects, sizeof(env.objects));
-	env.objects[0] = new_cone(vtx_new(5, 1, 1, 1),
+	env.objects[0] = new_cone(vtx_new(0, 0, 0, 1),
 			vtx_new(45 * M_PI / 180, 45 * M_PI / 180, 00 * M_PI / 180, 1),
 			vtx_new(0, 0, -105, 1),
 			new_color(0xFF, 0x88, 0x88, 0x00));
+	env.objects[1] = new_sphere(vtx_new(25, 25, 25, 1),
+			vtx_new(0 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(0, 0, -105, 1),
+			new_color(0xFF, 0xFF, 0x00, 0x00));
+	env.objects[2] = new_sphere(vtx_new(25, 25, 25, 1),
+			vtx_new(0 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(-15, 0, -105, 1),
+			new_color(0xFF, 0xFF, 0x00, 0x00));
+	env.objects[3] = new_sphere(vtx_new(25, 25, 25, 1),
+			vtx_new(0 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(15, 0, -105, 1),
+			new_color(0xFF, 0xFF, 0x00, 0x00));
+	env.objects[4] = new_cylinder(vtx_new(5, 6, 1, 1),
+			vtx_new(90 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(15, 0, -84, 1),
+			new_color(0x00, 0xFF, 0x00, 0x00));
+	env.objects[5] = new_plane(vtx_new(25, 25, 25, 1),
+			vtx_new(45 * M_PI / 180, 90 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(-200, 0, -105, 1),
+			new_color(0xFF, 0x00, 0x00, 0x00));
 	env.mlx = new_mlx();
 	env.win = new_window(env.mlx, env.camera.screen.x, env.camera.screen.y, "holy carp");
 	env.img = new_image(env.mlx, env.camera.screen.x, env.camera.screen.y);
