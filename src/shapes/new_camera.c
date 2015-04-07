08804404cb08732729bcf41805e1f0174f3b7be1
shakadak <nathanael.pineau@gmail.com>
2015-04-07 18:16:56 +0200

    field of view of 90 degree seems more natural

--- src/shapes/new_camera.c
+++ src/shapes/new_camera.c
@@ -6,7 +6,7 @@ t_camera	new_camera(t_mtx const pos, t_mtx const dir, t_pos const dim)
 	t_camera	camera;
 
 	camera.dir = mtx_dup(dir);
-	camera.fov = 60 * M_PI / 180;
+	camera.fov = 90 * M_PI / 180;
 	camera.pos = mtx_dup(pos);
 	camera.screen = dim;
 	return (camera);
