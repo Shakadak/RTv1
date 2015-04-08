3bde8e330f5d45b51a9605685ad89b56842ee313
shakadak <nathanael.pineau@gmail.com>
2015-04-08 17:46:46 +0200

    removed unusedsed counter

--- src/ray/ray_cast.c
+++ src/ray/ray_cast.c
@@ -17,7 +17,6 @@ t_color	ray_cast(t_pos const pos,
 	distance = DBL_MAX;
 	ft_bzero(&nearest, sizeof(nearest));
 	ray = new_ray(camera, pos);
-	int	i = 0;
 	while (objects->defined)
 	{
 		t_ray = ray_transform(ray, objects->pipe);
@@ -27,7 +26,6 @@ t_color	ray_cast(t_pos const pos,
 			distance = t_distance;
 			nearest = *objects;
 		}
-		++i;
 		++objects;
 	}
 	return (nearest.rgb);
