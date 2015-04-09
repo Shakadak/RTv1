8cad7b9704aeb1889dc2e41f8a454c93834abfe5
shakadak <nathanael.pineau@gmail.com>
2015-04-09 17:21:34 +0200

    shading started

--- src/ray/ray_shade.c
+++ src/ray/ray_shade.c
@@ -4,4 +4,19 @@ t_color	ray_shade(t_object const obj,
 		t_ray const ray,
 		double const dist,
 		t_object const *objects,
-		t_light const *light)
+		t_light const *lights)
+{
+	t_ray	normal;
+	t_ray	l_ray;
+	double	dot;
+
+	normal = ray_normal(obj, ray, dist);
+	while (lights->defined)
+	{
+		l_ray.dir = mtx_sub(lights->pos, normal.pos);
+		dot = vec_dot(normal.dir, l_ray);
+		if (dot >= 1 || dot < 0)
+			continue;
+		++lights;
+	}
+}
--- src/ray/sources.mk
+++ src/ray/sources.mk
@@ -1,6 +1,7 @@
 RAY	:=	ray_cast.c \
 		ray_new.c \
 		ray_normal.c \
+		ray_shade.c \
 		ray_transform.c
 
 OBJ := $(OBJ) $(RAY:.c=.o)
