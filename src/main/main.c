f8b523617ae49f4156b5cfaea547b00968ce9f0c
shakadak <nathanael.pineau@gmail.com>
2015-05-11 16:48:41 +0200

    still struggling with surfaces normal

--- src/main/main.c
+++ src/main/main.c
@@ -28,32 +28,32 @@ int	main(void)
 	t_env	env;
 
 	ft_bzero(&env, sizeof(env));
-	env.camera = new_camera(vtx_new(0, 1, 0, 1), vtx_new(0, 0, -1, 1), new_pos(1350, 750, 0));/*
-	env.objects[0] = new_cone(vtx_new(1, 1, 1.0, 1),
+	env.camera = new_camera(vtx_new(0, 0, -20, 1), vtx_new(0, 0, -1, 1), new_pos(1350, 750, 0));
+	env.objects[6] = new_cone(vtx_new(1, 1, 1.0, 1),
 			vtx_new(90 * M_PI / 180, 00 * M_PI / 180, 00 * M_PI / 180, 1),
 			vtx_new(0, 0, -105, 1),
-			new_color(0xFF, 0x88, 0x88, 0x00));*/
+			new_color(0xFF, 0x00, 0x00, 0x00));
 	env.objects[1] = new_sphere(vtx_new(25, 25, 25, 1),
 			vtx_new(0 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
-			vtx_new(0, 0, -075, 1),
+			vtx_new(0, 0, -105, 1),
 			new_color(0xFF, 0xFF, 0x00, 0x00));
 	env.objects[2] = new_sphere(vtx_new(25, 25, 25, 1),
 			vtx_new(0 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
-			vtx_new(-15, 0, -075, 1),
+			vtx_new(-15, 0, -105, 1),
 			new_color(0xFF, 0xFF, 0x00, 0x00));
-	env.objects[0] = new_sphere(vtx_new(25, 25, 25, 1),
+	env.objects[3] = new_sphere(vtx_new(25, 25, 25, 1),
 			vtx_new(0 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
-			vtx_new(15, 0, -075, 1),
-			new_color(0xFF, 0xFF, 0x00, 0x00));/*
-	env.objects[4] = new_cylinder(vtx_new(5, 6, 1, 1),
+			vtx_new(15, 0, -105, 1),
+			new_color(0xFF, 0xFF, 0x00, 0x00));
+	env.objects[0] = new_cylinder(vtx_new(5, 5, 5, 1),
 			vtx_new(90 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
-			vtx_new(15, 0, -84, 1),
+			vtx_new(15, 0, -64, 1),
 			new_color(0x00, 0xFF, 0x00, 0x00));
-	env.objects[5] = new_plane(vtx_new(25, 25, 25, 1),
-			vtx_new(00 * M_PI / 180, 00 * M_PI / 180, 00 * M_PI / 180, 1),
+	env.objects[6] = new_plane(vtx_new(25, 25, 25, 1),
+			vtx_new(000 * M_PI / 180, 000 * M_PI / 180, 00 * M_PI / 180, 1),
 			vtx_new(-200, 0, -105, 1),
-			new_color(0xFF, 0x00, 0x00, 0x00));*/
-	env.lights[0] = new_light(vtx_new(0, 1, 0, 1));
+			new_color(0xFF, 0x00, 0x00, 0x00));
+	env.lights[0] = new_light(vtx_new(0, 0, 0, 1));
 	env.mlx = new_mlx();
 	env.win = new_window(env.mlx, env.camera.screen.x, env.camera.screen.y, "holy carp");
 	env.img = new_image(env.mlx, env.camera.screen.x, env.camera.screen.y);
--- src/ray/ray_shade.c
+++ src/ray/ray_shade.c
@@ -1,5 +1,5 @@
 #include "rtv1.h"
-#include "stdio.h"
+#include "stdio.h"//////////////
 
 static int	obstructed(t_object const *olist, t_ray const ray)
 {
@@ -9,7 +9,7 @@ static int	obstructed(t_object const *olist, t_ray const ray)
 	{
 		d = intersec_quadric(*olist, ray_transform(ray, olist->pipe));
 		printf("d = %f, ray.magnitude = %f\n", d, vec_magnitude(ray.dir));
-		if (0 + EPSILON < d || d < vec_magnitude(ray.dir) - EPSILON)
+		if (0 + EPSILON < d && d < vec_magnitude(ray.dir) - EPSILON)
 			return (1);
 		++olist;
 	}
@@ -29,16 +29,23 @@ t_color	ray_shade(t_object obj,
 	normal = ray_normal(obj, ray, dist);
 	while (lights->defined)
 	{
+		printf("normal.pos [%f,%f,%f] : light.pos [%f,%f,%f]\n", normal.pos.mtx[0],
+				normal.pos.mtx[1],
+				normal.pos.mtx[2],
+				l_ray.pos.mtx[0],
+				l_ray.pos.mtx[1],
+				l_ray.pos.mtx[2]);
 		l_ray.dir = mtx_sub(lights->pos, normal.pos);
 		dot = vec_dot(normal.dir, vec_normalize(l_ray.dir))/* / (vec_magnitude(normal.dir) * vec_magnitude(l_ray.dir))*/;
-/*		printf("normal.dir [%f,%f,%f] : light.dir [%f,%f,%f]\n", normal.dir.mtx[0],
+		printf("normal.dir [%f,%f,%f] : light.dir [%f,%f,%f]\n", normal.dir.mtx[0],
 				normal.dir.mtx[1],
 				normal.dir.mtx[2],
 				l_ray.dir.mtx[0],
 				l_ray.dir.mtx[1],
-				l_ray.dir.mtx[2]);*/
-		if (dot >= 1 + EPSILON || dot <= 0 - EPSILON)
+				l_ray.dir.mtx[2]);
+		if (!(dot <= 1 + EPSILON && dot >= 0 - EPSILON))
 		{
+			printf("dot = %f\n", dot);
 			ft_putendl_fd("antidot", 2);
 			++lights;
 			obj.rgb.rgb[0] /= 9;
@@ -47,6 +54,10 @@ t_color	ray_shade(t_object obj,
 			obj.rgb.rgb[3] /= 9;
 			continue;
 		}
+		obj.rgb.rgb[0] *= dot;
+		obj.rgb.rgb[1] *= dot;
+		obj.rgb.rgb[2] *= dot;
+		obj.rgb.rgb[3] *= dot;
 		l_ray.pos = normal.pos;
 		if (obstructed(olist, l_ray))
 		{
