871528c5fcf5e0f7ed6a9d80d85dcb7c96888483
shakadak <nathanael.pineau@gmail.com>
2015-05-15 17:53:22 +0200

    scenery

--- src/main/main.c
+++ src/main/main.c
@@ -28,7 +28,7 @@ int	main(void)
 	t_env	env;
 
 	ft_bzero(&env, sizeof(env));
-	env.camera = new_camera(vtx_new(0, 0, -20, 1), vtx_new(0, 0, -1, 1), new_pos(2000, 1000, 0));
+	env.camera = new_camera(vtx_new(0, 0, +0, 1), vtx_new(0, 0, -1, 1), new_pos(2000, 1000, 0));
 	env.objects[0] = new_cone(vtx_new(1, 1, 1.0, 1),
 			vtx_new(90 * M_PI / 180, 00 * M_PI / 180, 00 * M_PI / 180, 1),
 			vtx_new(0, 0, -105, 1),
@@ -49,14 +49,22 @@ int	main(void)
 			vtx_new(90 * M_PI / 180, 0 * M_PI / 180, 00 * M_PI / 180, 1),
 			vtx_new(15, 0, -64, 1),
 			new_color(0x00, 0xFF, 0x00, 0x00));
-	env.objects[5] = new_plane(vtx_new(25, 25, 25, 1),
+	env.objects[5] = new_cylinder(vtx_new(4, 4, 4, 1),
+			vtx_new(90 * M_PI / 180, 0 * M_PI / 180, 90 * M_PI / 180, 1),
+			vtx_new(15, 24, -64, 1),
+			new_color(0x00, 0xFF, 0x00, 0x00));
+	env.objects[6] = new_plane(vtx_new(25, 25, 25, 1),
 			vtx_new(000 * M_PI / 180, 000 * M_PI / 180, 00 * M_PI / 180, 1),
 			vtx_new(0, 0, -205, 1),
 			new_color(0x00, 0x00, 0xFF, 0x00));
-	env.objects[6] = new_cylinder(vtx_new(4, 4, 4, 1),
-			vtx_new(90 * M_PI / 180, 0 * M_PI / 180, 90 * M_PI / 180, 1),
-			vtx_new(15, 50, -64, 1),
-			new_color(0x00, 0xFF, 0x00, 0x00));
+	env.objects[7] = new_plane(vtx_new(25, 25, 25, 1),
+			vtx_new(000 * M_PI / 180, 80 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(80, 0, -205, 1),
+			new_color(0x00, 0x00, 0xFF, 0x00));
+	env.objects[8] = new_plane(vtx_new(25, 25, 25, 1),
+			vtx_new(000 * M_PI / 180, -80 * M_PI / 180, 00 * M_PI / 180, 1),
+			vtx_new(-80, 0, -205, 1),
+			new_color(0x00, 0x00, 0xFF, 0x00));
 	env.lights[0] = new_light(vtx_new(0, 0, 0, 1));
 	env.lights[1] = new_light(vtx_new(90, 0, +75, 1));
 	env.lights[0] = new_light(vtx_new(-70, 0, -85, 1));
