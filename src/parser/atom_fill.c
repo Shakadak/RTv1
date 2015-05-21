bb40b1829d3f54c66c975eece94973c898be3980
shakadak <nathanael.pineau@gmail.com>
2015-05-21 17:43:14 +0200

    onward

--- src/parser/atom_fill.c
+++ src/parser/atom_fill.c
@@ -1,25 +1,43 @@
 #include "rtv1.h"
 
-static t_atom	(*get_fill(char const *const src))(t_atom, char const *const *const, int *)
+static t_atom	get_fill(t_atom atom, char const *const *const src, int *i)
 {
-	(void)src;
-	return (fill_type);
+	if (!ft_strcmp(src[*i], "CAMERA"))
+		atom.type = CAMERA;
+	else if (!ft_strcmp(src[*i], "LIGHT"))
+		atom.type = LIGHT;
+	else if (!ft_strcmp(src[*i], "CYLINDER"))
+		atom.type = CYLINDRE;
+	else if (!ft_strcmp(src[*i], "CONE"))
+		atom.type = CONE;
+	else if (!ft_strcmp(src[*i], "PLANE"))
+		atom.type = PLANE;
+	else if (!ft_strcmp(src[*i], "SPHERE"))
+		atom.type = SPHERE;
+	else if (!ft_strcmp(src[*i], "SCALE"))
+		atom.scale = fill_scale(src, i);
+	else if (!ft_strcmp(src[*i], "ROTATION"))
+		atom.rotation = fill_rotation(src, i);
+	else if (!ft_strcmp(src[*i], "TRANSLATION"))
+		atom.translation = fill_translation(src, i);
+	else if (!ft_strcmp(src[*i], "COLOR"))
+		atom.translation = fill_color(src, i);
+	else
+		ft_fatal("Error while parsing file.\n", 0);
+	++(*i);
+	return (atom);
 }
 
-t_atom	atom_fill(char const *const *const array)
+t_atom			atom_fill(char const *const *const array)
 {
 	int		i;
 	t_atom	atom;
-	t_atom	(*fill)(t_atom, char const *const *const, int *);
 
 	i = 0;
 	atom = atom_new();
 	while (array[i] != NULL)
 	{
-		if ((fill = get_fill(array[i])) != NULL)
-			atom = fill(atom, array, &i);
-		else
-			ft_fatal("Error while parsing", 0);
+		atom = get_fill(atom, array, &i);
 	}
 	return (atom);
 }
