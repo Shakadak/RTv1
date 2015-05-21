253a118143da5851a82aa0e8265702480cdad6fd
shakadak <nathanael.pineau@gmail.com>
2015-05-21 20:36:44 +0200

    changed string number conversion type

--- src/parser/fill_color.c
+++ src/parser/fill_color.c
@@ -4,6 +4,6 @@ t_color	fill_color(char const *const *const src, int *i)
 {
 	if (ft_split_len(src + *i) < 4)
 		ft_fatal("Error while parsing color.\n", 0);
-	return (new_color(ft_atof(src[++(*i)]), ft_atof(src[++(*i)]),
-	ft_atof(src[++(*i)]), 0x00));
+	return (new_color(ft_atoi(src[++(*i)]), ft_atoi(src[++(*i)]),
+	ft_atoi(src[++(*i)]), 0x00));
 }
